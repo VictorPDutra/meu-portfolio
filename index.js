f70d3d23e5a494516fa7bab4da99b57518d33334
VictorPDutra <161891023+VictorPDutra@users.noreply.github.com>
2024-07-10 18:06:34 -0300

    implementação de scroll ao clicar

---
+++ index.js
@@ -0,0 +1,19 @@
+let links = document.querySelectorAll('.js-link');
+let sections = document.querySelectorAll('.section');
+
+function scrollSection(event) {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href)
+    let topSection = section.offsetTop - 72;
+    window.scrollTo({
+        top: topSection,
+        behavior: "smooth"
+    })
+}
+
+links.forEach(link => {
+    link.addEventListener('click', scrollSection)
+});
+
