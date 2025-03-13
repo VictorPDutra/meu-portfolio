bd34f1f3e1cd780f178908c4d3fdb94983938890
VictorPDutra <161891023+VictorPDutra@users.noreply.github.com>
2025-03-13 17:37:56 -0300

    refactor: Reestrutura diretórios
    Agora os arquivos estão mais bem divididos.
    Também as folhas de estilo estão mais bem divididas.
    Agora temos uma folha só para media queries.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let links = document.querySelectorAll(".js-link");
+let sections = document.querySelectorAll(".section");
+
+let mobileMenu = document.querySelector("#hamburguer");
+let navListMobile = document.querySelector(".nav-list");
+
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    let topSection = section.offsetTop - 72;
+    window.scrollTo({
+      top: topSection,
+      behavior: "smooth",
+    });
+
+    let navListMobile = document.querySelector(".nav-list");
+    let largura = window.innerWidth;
+    if (largura <= 660) {
+      navListMobile.style.display = "none";
+    }
+  });
+});
+
+mobileMenu.addEventListener("click", () => {
+  if (navListMobile.style.display == "none") {
+    navListMobile.style.display = "flex";
+  } else {
+    navListMobile.style.display = "none";
+  }
+});
+
+window.addEventListener("resize", () => {
+  let larg = window.innerWidth;
+
+  if (larg <= 660) {
+    navListMobile.style.display = "none";
+  } else {
+    navListMobile.style.display = "flex";
+  }
+});
